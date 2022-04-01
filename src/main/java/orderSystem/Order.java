8c159d1c79ee2af288ae0a9587c3f9e1c941c8de
mstuff <mstuff@posteo.de>
2022-04-01 14:52:51 +0200

    Class Order: added Attributes

--- src/main/java/orderSystem/Order.java
+++ src/main/java/orderSystem/Order.java
@@ -1,4 +1,17 @@
 package orderSystem;
 
 public class Order {
+    Product orderedProduct;
+    int numberOfOrderedProducts;
+
+    public Order(Product orderedProduct, int numberOfOrderedProducts){
+        this.orderedProduct = orderedProduct;
+        this.numberOfOrderedProducts = numberOfOrderedProducts;
+
+
+
+    }
+
+
+
 }
