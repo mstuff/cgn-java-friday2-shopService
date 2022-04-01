5c0e878b07f5c685146e22372cbc563f05a44351
mstuff <mstuff@posteo.de>
2022-04-01 12:41:13 +0200

    Class Product: added attributes, getter, setter, toString, equals, hashCode

--- src/main/java/orderSystem/Product.java
+++ src/main/java/orderSystem/Product.java
@@ -1,4 +1,80 @@
 package orderSystem;
 
+import java.util.Objects;
+
 public class Product {
+
+    private String productId;
+    private String productName;
+    private double price;
+    private int numberInStore;
+
+
+    public Product(){
+
+    }
+
+    public Product(String productId, String productName, double price, int numberInStore){
+        this.productId = productId;
+        this.productName = productName;
+        this.price = price;
+        this.numberInStore = numberInStore;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId='" + productId + '\'' +
+                ", productName='" + productName + '\'' +
+                ", price=" + price +
+                ", numberInStore=" + numberInStore +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product)) return false;
+        Product product = (Product) o;
+        return Double.compare(product.price, price) == 0 && numberInStore == product.numberInStore && Objects.equals(productId, product.productId) && Objects.equals(productName, product.productName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productId, productName, price, numberInStore);
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getNumberInStore() {
+        return numberInStore;
+    }
+
+    public void setNumberInStore(int numberInStore) {
+        this.numberInStore = numberInStore;
+    }
 }
