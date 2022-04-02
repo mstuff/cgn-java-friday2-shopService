2530f0b931f2ce24e63957e454c1a3030c552eec
mstuff <mstuff@posteo.de>
2022-04-02 11:53:55 +0200

    Refactor: create all classes and method signatures

--- src/main/java/orderSystem/UserInteraction.java
+++ src/main/java/orderSystem/UserInteraction.java
@@ -1,4 +1,11 @@
 package orderSystem;
 
 public class UserInteraction {
+    private String userPrompt;
+    private String userInput;
+
+    public UserInteraction (String userPrompt, String userInput){
+        this.userPrompt = userPrompt;
+        this.userInput = userInput;
+    }
 }
